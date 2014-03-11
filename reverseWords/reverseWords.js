06f0ec72c328659cc9b290d37b5ae14a80c42158
claudio <claudio.sousa@gmail.com>
2014-03-11 22:54:13 +0100

    initial codejam stuff

---
+++ reverseWords/reverseWords.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var inputFile = process.argv[2];
+
+var inputStr = fs.readFileSync(inputFile, { encoding: 'utf8' });
+
+var lines = inputStr.split('\n');
+var nbCases = Number(lines[0]);
+var cases = [];
+for (var i = 0; i < nbCases; i++) {
+    var lineI = i  + 1;
+    var newcase = {
+        input: lines[lineI]
+    }
+    cases.push(newcase);
+}
+
+for (var i = 0; i < cases.length; i++) {
+    var currentCase = cases[i];
+    currentCase.output = currentCase.input.split(' ');
+    currentCase.output.reverse();
+}
+for (var i = 0; i < cases.length; i++) {
+    console.log("Case #" + (i + 1) + ": " + cases[i].output.join(' '));
+}
+//Case #1: 2 3
