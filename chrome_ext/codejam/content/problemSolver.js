838982c397c3b1b0fbb0bd1ebf66a6daefbda9e1
claudio sousa <claudio.sousa@sungard.com>
2014-03-13 08:57:20 +0100

    messing up with code elements

---
+++ chrome_ext/codejam/content/problemSolver.js
@@ -0,0 +1,17 @@
+﻿problemSolver = (function () {
+    var problemSolver = {};
+    var codeElement = null;
+    problemSolver.setCodeElement = function (_codeElement) {
+        codeElement = _codeElement;
+        codeElement.innerText = "function solveInput(inputStr){\n\
+                                 \treturn prompt(inputStr);\n\
+                                }";
+    }
+
+    problemSolver.solveStrInput = function (strInput) {
+        var problemSolverFn = eval('(' + codeElement.innerText + ')');
+        return problemSolverFn(strInput)
+    }
+
+    return problemSolver;
+})()
