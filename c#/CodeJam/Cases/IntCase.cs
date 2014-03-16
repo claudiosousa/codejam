6740301b29237a4ee634583bcdd3e6bd87eaa10f
claudio <claudio.sousa@gmail.com>
2014-03-16 23:21:32 +0100

    Alien Language

---
+++ c#/CodeJam/Cases/IntCase.cs
@@ -0,0 +1,29 @@
+﻿class Case
+{
+    public int[][] input;
+    public string output;
+
+    public static Case[] parseinput(string input)
+    {
+        string[] lines = input.Trim().Split('\n');
+
+        long nbCases = Convert.ToInt64(lines[0]);
+        long linesPerCase = (lines.Length - 1) / nbCases;
+
+        Case[] cases = new Case[nbCases];
+
+        for (int i = 0; i < nbCases; i++)
+        {
+            var caseLine = i * linesPerCase + 1;
+            Case newcase = new Case { input = new int[linesPerCase][] };
+
+            for (var iLine = 0; iLine < linesPerCase; iLine++)
+            {
+                string[] lineParts = lines[caseLine + iLine].Split(' ');
+                newcase.input[iLine] = lineParts.Select(p => Convert.ToInt32(p)).ToArray();
+            }
+            cases[i] = newcase;
+        }
+        return cases;
+    }
+}
