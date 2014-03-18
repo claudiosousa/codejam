8649be5d8fba3333ee2cb593807daa6160046c9a
claudio sousa <claudio.sousa@sungard.com>
2014-03-18 19:02:00 +0100

    star working on the chess board

--- c#/CodeJam/Solver.cs
+++ c#/CodeJam/Solver.cs
@@ -11,35 +11,15 @@ namespace CodeJam
 
     class Solver
     {
-
         static void processCase(Case cas)
         {
-            long L = cas.input[0];
-            long P = cas.input[1];
-            long C = cas.input[2];
-            double PoL = P / L;
-
-
-            int slots = 0;
-            long coveredAmount = Math.Max(1, L);
-            while (coveredAmount * C < P)
-            {
-                coveredAmount *= C;
-                slots++;
-            }
-            if (slots == 0)
-                cas.output = "0";
-            else
-                cas.output = ""+ Convert.ToString(slots, 2).Length;          
         }
-
 
         public static string Solve(string input)
         {
             Case[] cases = Case.parseinput(input);
             for (int i = 0; i < cases.Length; i++)
             {
-                Console.WriteLine("Case: " + i);
                 processCase(cases[i]);
             }
             return writeOutput(cases);
@@ -58,22 +38,45 @@ namespace CodeJam
 
         class Case
         {
-            public int[] input;
+            public int[][] input;
             public string output;
 
             public static Case[] parseinput(string input)
             {
-                string[] lines = input.Trim().Split('\n').Select(l => l.TrimEnd('\r')).ToArray();
+                string[] lines = input.Trim().Split('\n');
 
-                long nbCases = Convert.ToInt64(lines[0]);
+                int nbCases = Convert.ToInt32(lines[0]);
                 Case[] cases = new Case[nbCases];
 
+                int iLine = 1;
                 for (int i = 0; i < nbCases; i++)
                 {
-                    var caseLine = i + 1;
-                    var lineParts = lines[caseLine].Split(' ');
-                    Case newcase = new Case { input = lineParts.Select(n => Convert.ToInt32(n)).ToArray() };
+                    int[] arraySize = lines[iLine].Split(' ').Select(s => Convert.ToInt32(s)).ToArray();
+
+                    Case newcase = new Case { input = new int[arraySize[0]][] };
+
+                    for (var iArrayLine = 0; iArrayLine < arraySize[0]; iArrayLine++)
+                    {
+                        iLine++;
+
+                        int[] lineBitArray = new int[arraySize[1]];
+                        string lineHex = lines[iLine];
+                        Regex.Replace(lineHex, "5", "001";);
+
+                        
+                        int lineBitArrayOffset = 0;
+                        for (int iHex = 0; iHex < lineHex.Length; iHex++)
+                        {
+                            string binaryLine = Convert.ToString(Convert.ToInt32("" + lineHex[iHex], 16), 2);
+                            lineBitArrayOffset += 4;
+                            for (int iByte = binaryLine.Length-1; iByte>=0; iByte--)
+                            {
+                                lineBitArray[lineBitArrayOffset - iByte] = binaryLine[binaryLine.Length - 1 - iByte];
+                            }
+                        }
+                    }
                     cases[i] = newcase;
+                    iLine++;
                 }
                 return cases;
             }
