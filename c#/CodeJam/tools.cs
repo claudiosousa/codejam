ed6980d2aaddbf31884bf00e236dcd7d20b57a0c
claudio <claudio.sousa@gmail.com>
2014-03-20 00:44:09 +0100

    solved "Join-K"

---
+++ c#/CodeJam/tools.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeJam
+{
+    static class tools
+    {
+        public static string printArray(Array arr)
+        {
+            string res = "\r\n";
+            var x = arr.GetLength(0);
+            var y = arr.GetLength(1);
+            for (int j = 0; j < y; j++)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    res+=arr.GetValue(i,j) + "";
+                }
+                res += "\r\n";
+            }
+            return res;
+        }
+    }
+}
