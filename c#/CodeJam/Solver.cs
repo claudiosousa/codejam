817ddc480bbccdcb286fc6567537b8ee8988c308
claudio sousa <claudio.sousa@sungard.com>
2014-04-28 11:08:11 +0200

    :(

--- c#/CodeJam/Solver.cs
+++ c#/CodeJam/Solver.cs
@@ -11,9 +11,150 @@ namespace CodeJam
 
     class Solver
     {
+        Dictionary<int, List<int>> tree;
+        Dictionary<string, int> dp;
+
+        int getMinDeletes(HashSet<int> removed, List<int[]> startNodes, int maxDeletes)
+        {
+            /*
+            var removedList = removed.ToList();
+            removedList.Sort();
+            var removedListKey = String.Join(",", removedList);
+            if (dp.ContainsKey(removedListKey))
+                return dp[removedListKey];
+             * */
+            List<int[]> nodes2verify = new List<int[]>();
+            for (int i = 0; i < startNodes.Count; i++)
+            {
+                nodes2verify.Add(startNodes[i]);
+            }
+            do
+            {
+                var node2verify = nodes2verify.Last();
+                nodes2verify.RemoveAt(nodes2verify.Count - 1);
+                int node = node2verify[0];
+                int nodeParent = node2verify[1];
+                var isRemovedNode = removed.Contains(nodeParent);
+                if (isRemovedNode)
+                    removed.Add(node);
+                if (removed.Count > maxDeletes)
+                {
+                 //   dp[removedListKey] = -1;
+                    return -1;
+                }
+                int[] nodeEdges = tree[node].ToArray();
+                nodeEdges = nodeEdges.Where(e => e != nodeParent && !removed.Contains(e)).ToArray();
+
+                if (!isRemovedNode && nodeEdges.Count() > 2)
+                {
+                    int nodes2remove = nodeEdges.Count() - 2;
+                    List<int> toremove = new List<int>();
+                    for (int i = 0; i < nodes2remove; i++)
+                    {
+                        toremove.Add(i);
+                    }
+                    HashSet<int> bestRemove = null;
+                    int minDeleted = maxDeletes - removed.Count;
+                    do
+                    {
+                        var tempRemoved = new HashSet<int>(removed);
+                        List<int[]> removedStartNodes = new List<int[]>();
+                        for (int i = 0; i < nodes2remove; i++)
+                        {
+                            removedStartNodes.Add(new int[] { nodeEdges[toremove[i]], node });
+                            tempRemoved.Add(nodeEdges[toremove[i]]);
+                        }
+
+                        var deleted = getMinDeletes(tempRemoved, removedStartNodes, minDeleted);
+                        if (deleted >= 0 && (bestRemove == null || tempRemoved.Count < bestRemove.Count))
+                        {
+                            bestRemove = tempRemoved;
+                            minDeleted = bestRemove.Count;
+                        }
+                        var isValidConbination = false;
+                        for (int i = nodes2remove - 1; i >= 0; i--)
+                        {
+                            var removeI = toremove[i];
+                            removeI++;
+                            toremove[i] = removeI;
+                            if (removeI < nodeEdges.Count() + i - nodes2remove + 1)
+                            {
+                                isValidConbination = true;
+                                for (int j = i + 1; j < nodes2remove; j++)
+                                    toremove[j] = removeI + j - i;
+                                break;
+                            }
+                        }
+                        if (!isValidConbination)
+                            break;
+                    } while (true);
+                    if (bestRemove == null)
+                    {
+                      //  dp[removedListKey] = -1;
+                        return -1;
+                    }
+                    var bestRemoveList = bestRemove.ToList();
+                    for (int i = 0; i < bestRemoveList.Count; i++)
+                    {
+                        if (!removed.Contains(bestRemoveList[i]))
+                            removed.Add(bestRemoveList[i]);
+                    }
+                    if (removed.Count > maxDeletes)
+                    {
+                    //    dp[removedListKey] = -1;
+                        return -1;
+                    }
+                }
+                else if (!isRemovedNode && nodeEdges.Count() == 1)
+                    removed.Add(nodeEdges[0]);
+
+                for (int i = 0; i < nodeEdges.Count(); i++)
+                {
+                    nodes2verify.Add(new int[] { nodeEdges[i], node });
+                }
+            } while (nodes2verify.Count > 0);
+          //  dp[removedListKey] = removed.Count;
+            return removed.Count;
+        }
+
+        int getMinDeletes()
+        {
+            int minDeletes = tree.Keys.Count - 1;
+            HashSet<int> minDeletesDeleted = null;
+            foreach (int key in tree.Keys)
+            {
+                dp = new Dictionary<string, int>();
+                var deleteed = new HashSet<int>();
+                var start = new List<int[]>();
+                start.Add(new int[] { key, 0 });
+                int keyMinDeletes = getMinDeletes(deleteed, start, minDeletes);
+
+                if (keyMinDeletes == 0)
+                    return 0;
+                if (keyMinDeletes > 0 && keyMinDeletes < minDeletes)
+                {
+                    minDeletes = keyMinDeletes;
+                    minDeletesDeleted = deleteed;
+                }
+            }
+            return minDeletes;
+        }
+
         string solveCase(int[][] input)
         {
-            return "0";
+            int N = input[0][0];
+            tree = new Dictionary<int, List<int>>();
+            for (int i = 1; i <= N; i++)
+            {
+                tree.Add(i, new List<int>());
+            }
+            for (int i = 1; i < input.Length; i++)
+            {
+                int[] edge = input[i];
+                tree[edge[0]].Add(edge[1]);
+                tree[edge[1]].Add(edge[0]);
+            }
+            return getMinDeletes() + "";
         }
 
 
@@ -26,12 +167,12 @@ namespace CodeJam
             int iLine = 1;
             for (int i = 0; i < nbCases; i++)
             {
-                //Console.WriteLine("Case: " + i);
+                Console.WriteLine("Case: " + i);
 
                 string[] lineParts = lines[iLine].Split(' ');
                 int[] linePartsint = lineParts.Select(p => Convert.ToInt32(p)).ToArray();
 
-                int caseLines = linePartsint[0];
+                int caseLines = linePartsint[0] - 1;
                 int[][] caseInput = new int[caseLines + 1][];
                 caseInput[0] = linePartsint;
 
@@ -45,6 +186,8 @@ namespace CodeJam
                 string result = solveCase(caseInput);
                 sb.AppendLine("Case #" + (i + 1) + ": " + result);
                 iLine++;
+                if (iLine >= lines.Length)
+                    break;
             }
             return sb.ToString();
         }
