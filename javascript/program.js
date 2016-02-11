54cc51c27dd9ae497c133e2d8bd6eabc5ed6b958
claudio <claudio.sousa@gmail.com>
2016-02-11 20:51:35 +0100

    mother of all houses

--- javascript/program.js
+++ javascript/program.js
@@ -1,151 +1,171 @@
 var fs = require('fs');
-var datastr = fs.readFileSync('learn_and_teach.in', "utf8").split('\n');
+var datastr = fs.readFileSync('mother_of_all_warehouses.in', "utf8").split('\n');
 
 var size = datastr[0].split(' ');
-var cols = Number(size[1]);
 var rows = Number(size[0]);
+var cols = Number(size[1]);
+var d_count = Number(size[2]);
+var deadline = Number(size[3]);
+var maxload = Number(size[4]);
+var p_count = Number(datastr[1]);
+var p_weights = datastr[2].split(' ').map(Number);
+var w_count = Number(datastr[3]);
+var w_coords = [];
+var w_items = [];
 
-var data = datastr.slice(1).map(l=> l.split(''));
-var datacopy = JSON.parse(JSON.stringify(data));
-var blocks = [];
-var buildBlock = function (r, c) {
-    var pieces = [];
-    console.log(blocks.length);
-    var minc, minr, maxc, maxr;
-    minc = maxc = c;
-    minr = maxr = r;
-    var addPiece = function (r, c) {
-        pieces.push([r, c]);
-        if (c < minc) minc = c;
-        if (c > maxc) maxc = c;
-        if (r < minr) minr = r;
-        if (r > maxr) maxr = r;
-        data[r][c] = '0';
-        var dir = [-1, 0, 1];
-        for (var x of dir) {
-            var c2 = c + x;
-            if (c2 < 0 || c2 >= cols)
-                continue;
-            for (var y of dir) {
-                var r2 = r + y;
-                if (r2 < 0 || r2 >= rows)
-                    continue;
-                if (data[r2][c2] !== '#')
-                    continue;
-                addPiece(r2, c2);
-            }
-        }
-    };
-    addPiece(r, c);
+var currentLine = 3;
+for (var i = 0; i < w_count; i++) {
+    currentLine++;
+    w_coords.push(datastr[currentLine].split(' ').map(Number));
+    currentLine++;
+    w_items.push(datastr[currentLine].split(' ').map(Number));
+}
 
-    var blockdata = [];
-    var rows = maxr - minr + 1;
-    var cols = maxc - minc + 1;
-    blocks.push([minr, minc, rows, cols, blockdata]);
-    for (var piece of pieces)
-        blockdata[(piece[0] - minr) * cols + (piece[1] - minc)] = 1;
+var c_count = Number(datastr[++currentLine]);
+var c = [];
+var c_prods = [];
+for (var i = 0; i < c_count; i++) {
+    currentLine++;
+    var coords = datastr[currentLine].split(' ').map(Number);
+    currentLine++;
+    currentLine++;
+    var prods = [];
+    datastr[currentLine].split(' ').map(Number).forEach(p=> {
+        if (!prods[p])
+            prods[p] = 1;
+        else
+            prods[p]++;
+    });
+    c_prods.push({ id: i, coords: coords, prods: prods });
+}
+
+
+
+/*
+fs.writeFileSync('data.json', JSON.stringify({
+    
+}));
+*/
+
+/*
+fs.writeFileSync('data.json', JSON.stringify({
+    c_prods
+}));
+*/
+
+var getDistance = function (c1, c2) {
+    return Math.ceil(Math.sqrt(Math.pow(c1[0] - c2[0], 2) + Math.pow(c1[1] - c2[1], 2)));
 };
 
-for (var r = 0; r < rows; r++) {
-    for (var c = 0; c < cols; c++) {
-        if (data[r][c] !== '#')
-            continue;
-        buildBlock(r, c);
+
+c_prods = c_prods.sort((a, b) => {
+    var getval = function (c) {
+        return getDistance(w_coords[0], c.coords) + c.prods.length;
     }
-}
+    return getval(a) - getval(b);
+})
+
+
+var tasks = [];
+
+c_prods.forEach(c=> {
+    c.prods.forEach((nb, p) => {
+        tasks.push({ id: c.id, coords: c.coords, p: p, nb: nb });
+    });
+});
 
 var commands = [];
 
-var decomposeBlock = function (block) {
-    var startr = block[0];
-    var startc = block[1];
-    var rows = block[2];
-    var cols = block[3];
-    var data = block[4];
-
-    var fillLine = function (r, c, data, isLine) {
-        var res = { score: 0, data: data, command: "PAINT_LINE " + (startr + r) + " " + (startc + c) + " " };
-        var lasrvalidrc;
-        while (r < rows && c < cols && data[r * cols + c] === 1) {
-            res.score++;
-            data[r * cols + c] = 2;
-            lasrvalidrc = [r, c];
-            if (isLine)
-                c++;
-            else
-                r++;
-        }
-        res.command += (startr + lasrvalidrc[0]) + " " + (startc + lasrvalidrc[1]);
-        return res;
-    };
-
-    var addLine = function (r, c, data) {
-        return fillLine(r, c, data, true);
-    }
-
-    var addCol = function (r, c, data) {
-        return fillLine(r, c, data, true);
-    };
-
-    var fillBlockCell = function (r, c) {
-        var res_line = addLine(r, c, data.slice());
-        var res_col = addCol(r, c, data.slice());
-        var bestRes = res_line.score > res_col.score ? res_line : res_col
-        commands.push(bestRes.command);
-
-        var parts = bestRes.command.split(' ').map(Number);
-        var y1 = parts[1], x1 = parts[2], y2 = parts[3], x2 = parts[4];
-
-        for (var x = x1; x <= x2; x++) {
-            for (var y = y1; y <= y2; y++) {
-                datacopy[y][x] = 'O';
-            }
-        }
-        data = bestRes.data;
-    };
-
-    for (var r = 0; r < rows; r++) {
-        for (var c = 0; c < cols; c++) {
-            if (data[r * cols + c] !== 1)
-                continue;
-            fillBlockCell(r, c, data);
-        }
-    }
-};
-/*
-var i = 0;
-var writeBlock = function (block) {
-    var rows = block[2];
-    var cols = block[3];
-    var data = block[4];
-    var res = "";
-    for (var r = 0; r < rows; r++) {
-        for (var c = 0; c < cols; c++) {
-            if (data[r * cols + c] !== 1)
-                res += " ";
-            else
-                res += "#";
-        }
-        res+='\n';
-    }
-    fs.writeFileSync('block' + i + '.out', res);
-    i++;
-};
-*/
-for (var block of blocks) {
-    //writeBlock(block);
-    decomposeBlock(block);
+var turns = 0;
+var drones = [];
+for (var i = 0; i < d_count; i++) {
+    drones.push({
+        turns: 0,
+        coords: [0, 0]
+    });
 }
 
+var processOrders = function () {
+    var donesomething = true;
+    while (donesomething && tasks.length > 0) {
+        donesomething = false;
+        for (var i = 0; i < d_count; i++) {
+            var drone = drones[i];
+            var dist_to_w = getDistance(drone.coords, w_coords[0]);
+            var task = tasks[0];
+            drone.coords = w_coords[0];
+            var dist_to_c = getDistance(drone.coords, task.coords);
+            drone.turns += dist_to_w + 1 + dist_to_c + 1;
+            var pcarried = [];
+            var dropCommands = [];
+            var writeCommands = function () {
+                pcarried.forEach((nb, p) => {
+                    if (nb > 0)
+                        commands.push(i + " L 0 " + p + " " + nb);
+                });
+                dropCommands.forEach(c => {
+                    if (c.nb > 0)
+                        commands.push(i + " D " + c.task.id + " " + c.task.p + " " + c.nb);
+                });
+            }
+            if (drone.turns < deadline) {
+
+                var w = p_weights[task.p];
+                var w_valid = 0;
+                var p_w = p_weights[task.p];
+
+                pcarried[task.p] = 0;
+                var dropCommand = { task: task, nb: 0 };
+                dropCommands.push(dropCommand);
+                while (w <= maxload && drone.turns < deadline) {
+                    drone.coords = task.coords;
+                    w_valid = w;
+                    pcarried[task.p]++;
+                    dropCommand.nb++;
+                    task.nb--;
+                    if (task.nb == 0) {
+                        tasks.shift();
+                        if (tasks.length == 0) {
+                            writeCommands();
+                            return;
+                        }
+                        task = tasks[0];
+                        p_w = p_weights[task.p];
+                        if (!pcarried[task.p]) {
+                            pcarried[task.p] = 0;
+                            drone.turns + 1;
+                        }
+                        dropCommand = { task: task, nb: 0 };
+                        dropCommands.push(dropCommand);
+                        drone.turns += getDistance(drone.coords, task.coords);
+                    }
+                    w += p_w;
+                }
+                donesomething = true;
+            }
+            if (donesomething) {
+                writeCommands();
+            }
+        }
+        /*
+        sortDronesByTurns desc
+        */
+    }
+}
+processOrders();
 commands.splice(0, 0, commands.length);
 fs.writeFileSync('output.out', commands.join('\n'));
-fs.writeFileSync('output2.out', datacopy.map(l=> l.join('')).join('\n'));
 
 process.exit(0);
+    
+/*Load/Deliver/Unload
 
-/*
-'PAINT_SQUARE'
-'PAINT_LINE'
-'ERASE_CELL'
+0 L 1 2 3
+
+ the ID of the drone that the command is for
+● the command tag ­ a single character, either ‘L’ (for load) or ‘U’ (for unload),
+● the ID of the warehouse from which we load items / to which we unload items
+● the ID of the product type
+● the number of items of the product type to be loaded or unloaded ­ a positive integer
 
 */
