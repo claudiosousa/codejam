db6269fcf62e2c3ca666626ab3099b29081f0727
claudio <claudio.sousa@epfl.ch>
2017-04-24 08:38:23 +0200

    round B

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,11 +1,5 @@
 const getStdin = require('get-stdin'),
-    fsp = require('fs-promise'),
-    math = require('mathjs');
-
-math.config({
-    number: 'BigNumber',
-    precision: 20
-});
+    fsp = require('fs-promise');
 
 (process.argv.length > 2 ? fsp.readFile(process.argv[2], { encoding: 'utf8' }) : getStdin())
     .then(parseCases)
@@ -23,18 +17,47 @@ function parseCases(inputStr) {
     let lines = inputStr.trim().split('\n');
     let nbCases = Number(lines[0]);
     let cases = [];
-
+    let offset = 0;
     for (var i = 0; i < nbCases; i++) {
-        let lineParts = lines[i + 1].split(' ');
-        cases.push([math.bignumber(lineParts[0]), math.bignumber(lineParts[1])]);
+        let [cities, routes] = lines[i + 1 + offset].split(' ').map(Number);
+        let horses = [];
+        for (var j = 1; j < cities; j++) {
+            horses.push(lines[i + 1 + offset + j].split(' ').map(Number));
+        }
+        offset += cities;
+        for (var j = 1; j < cities; j++) {
+            lineParts = lines[i + 1 + offset + j].split(' ').map(Number);
+            horses[j - 1].push(lineParts[j]);
+        }
+        offset += cities + 1;
+        cases.push(horses);
     }
 
     return cases;
+
+}
+let hs;
+function processCase(horses, ic) {
+    hs = horses.map(h => ({
+        e: h[0],
+        s: h[1],
+        d: h[2]
+    }));
+    let h1 = hs[0];
+    return getbesttime(0, h1.d / h1.s, h1.e - h1.d, h1.s);
 }
 
-function processCase([N, K], i) {
-    let powerof2 = math.bignumber(2).pow(math.bignumber(K).log(2).floor());
-    let free = N.sub(K.sub(1)).div(powerof2).ceil();
-    free = free.sub(1).div(2);
-    return free.ceil().toNumber() + " " + free.floor().toNumber();
+
+function getbesttime(i, time, hdist, hspeed) {
+    i++;
+    if (hs.length == i)
+        return time;
+    let h = hs[i];
+
+    let t0 = Number.MAX_VALUE, t1 = Number.MAX_VALUE;
+    if (hdist >= h.d)
+        t0 = getbesttime(i, h.d / hspeed, hdist - h.d, hspeed);
+    if (h.s > hspeed || h.e > hdist)
+        t1 = getbesttime(i, h.d / h.s, h.e - h.d, h.s);
+    return time + Math.min(t0, t1);
 }
