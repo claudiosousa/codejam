780492a4e0419fa4d1dd6ab2f878f8b3ab05cd97
Claudio <claudio.sousa@gmail.com>
2017-05-04 18:40:55 +0200

    nodejs

--- nodejs/app.js
+++ nodejs/app.js
@@ -16,48 +16,116 @@ function printCasesOutput(outputs) {
 function parseCases(inputStr) {
     let lines = inputStr.trim().split('\n');
     let nbCases = Number(lines[0]);
-    let cases = [];
     let offset = 0;
+    let cases = [];
     for (var i = 0; i < nbCases; i++) {
-        let [cities, routes] = lines[i + 1 + offset].split(' ').map(Number);
-        let horses = [];
-        for (var j = 1; j < cities; j++) {
-            horses.push(lines[i + 1 + offset + j].split(' ').map(Number));
+        let [t1, t2] = lines[i + 1 + offset].split(' ').map(Number);
+        let tasks = [];
+        for (var j = 1; j <= t1 + t2; j++) {
+            let [s, e] = lines[i + 1 + offset + j].split(' ').map(Number);
+            tasks.push({
+                t1: j <= t1,
+                s,
+                e,
+                t: e - s
+            });
         }
-        offset += cities;
-        for (var j = 1; j < cities; j++) {
-            lineParts = lines[i + 1 + offset + j].split(' ').map(Number);
-            horses[j - 1].push(lineParts[j]);
-        }
-        offset += cities + 1;
-        cases.push(horses);
+        offset += t1 + t2;
+        cases.push(
+            tasks
+        );
     }
 
     return cases;
 
 }
-let hs;
-function processCase(horses, ic) {
-    hs = horses.map(h => ({
-        e: h[0],
-        s: h[1],
-        d: h[2]
-    }));
-    let h1 = hs[0];
-    return getbesttime(0, h1.d / h1.s, h1.e - h1.d, h1.s);
+
+
+function processCase(tasks, ic) {
+    return calculate(tasks);
 }
 
+function calculate(tasks, ic) {
+    let retry;
+    let time1 = 0;
+    let time2 = 0;
+    let mandatoryChnages;
+    do {
+        tasks.sort((a, b) => a.s - b.s);
+        for (let i = 0; i < tasks.length - 1; i++) {
+            if (tasks[i + 1].t1 != tasks[i].t1)
+                tasks[i].i = 0;
+            else
+                tasks[i].i = tasks[i + 1].s - tasks[i].e;
+        }
+        tasks[tasks.length - 1].i = 0;
+        time1 = 0;
+        time2 = 0;
+        retry = false;
+        mandatoryChnages = 0;
+        let change = false;
+        for (let i = 0; i < tasks.length; i++) {
+            change = i > 0 && tasks[i].t1 != tasks[i - 1].t1;
+            if (change)
+                mandatoryChnages++;
+            let time = tasks[i].t;
+            if (!change && i > 0) {
+                time += tasks[i - 1].i;
+            }
+            if (tasks[i].t1)
+                time1 += time;
+            else
+                time2 += time;
+        }
 
-function getbesttime(i, time, hdist, hspeed) {
-    i++;
-    if (hs.length == i)
-        return time;
-    let h = hs[i];
+        if (time1 > 720) {
+            splittasks(tasks, true);
+            retry = true;
+        }
+        if (time2 > 720) {
+            splittasks(tasks, false);
+            retry = true;
+        }
+    } while (retry);
 
-    let t0 = Number.MAX_VALUE, t1 = Number.MAX_VALUE;
-    if (hdist >= h.d)
-        t0 = getbesttime(i, h.d / hspeed, hdist - h.d, hspeed);
-    if (h.s > hspeed || h.e > hdist)
-        t1 = getbesttime(i, h.d / h.s, h.e - h.d, h.s);
-    return time + Math.min(t0, t1);
+
+    let lastIsT1 = tasks[tasks.length - 1].t1;
+    let time = lastIsT1 ? time1 : time2;
+    if (time + tasks[0].start + 1440 - tasks[tasks.length - 1].e > 720) {
+        lastIsT1 = !lastIsT1;
+        mandatoryChnages += 1;
+    }
+    if (lastIsT1 != tasks[0].t1)
+        mandatoryChnages += 1;
+    // console.log(mandatoryChnages + "\n" + JSON.stringify(tasks, null, '\t'));
+    return mandatoryChnages;
+}
+
+function splittasks(tasks, t1) {
+    tasks.sort((a, b) => {
+        if (a.t1 == t1 && b.t1 == t1) {
+            return b.i - a.i;
+        }
+        return b.t1 == t1 ? 1 : -1;
+    });
+    tasks.push({
+        s: tasks[0].e + 1,
+        e: tasks[0].e + 1,
+        t: 0,
+        t1: !t1
+    });
+
+}
+
+function getGroupCount(changes, t1, index, timet1, timet2) {
+    if (timet1 > 720 || timet2 > 720)
+        return Number.MAX_VALUE;
+    let task = tasks[index];
+    let time0 = Number.MAX_VALUE;
+    let time1 = Number.MAX_VALUE;
+    if (task.t1 == t1)
+        time0 = getGroupCount(changes, t1, index + 1, t1 ? (timet1 + task.t) : timet1, !t1 ? (timet2 + task.t) : timet2);
+
+    time1 = getGroupCount(changes + 1, t1, index + 1);
+    return changes + Math.min(time0, time1);
 }
